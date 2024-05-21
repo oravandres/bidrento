d44879e93c51c5d4a8f52fd7edc6361de63dfa95
Andres Orav <andres.orav@gmail.com>
2024-05-21 22:52:48 +0300

    Add main app and css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PropertyTree from './components/PropertyTree';
+import AddProperty from './components/AddProperty';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<PropertyTree />} />
+                    <Route path="/add-property" element={<AddProperty />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;

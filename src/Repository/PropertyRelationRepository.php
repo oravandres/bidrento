315ee8fdccaa214a88b0776c743990d16d57f0e3
Andres Orav <andres.orav@gmail.com>
2024-05-28 01:41:12 +0300

    Add comments to repositories

--- src/Repository/PropertyRelationRepository.php
+++ src/Repository/PropertyRelationRepository.php
@@ -11,6 +11,9 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PropertyRelationRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry The ManagerRegistry instance.
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PropertyRelation::class);

b5d0e34c0d827893cd8b44106ed5ec92b2ac6237
Andres Orav <andres.orav@gmail.com>
2024-05-28 01:49:17 +0300

    Comment dto

--- src/DTO/CreateOrUpdateRequest.php
+++ src/DTO/CreateOrUpdateRequest.php
@@ -5,8 +5,16 @@ namespace App\DTO;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Annotation\SerializedName;
 
+/**
+ * Data Transfer Object for creating or updating a property.
+ */
 class CreateOrUpdateRequest
 {
+    /**
+     * The name of the property.
+     *
+     * @var string
+     */
     #[Assert\Length(
         min: 1,
         max: 255,
@@ -16,12 +24,29 @@ class CreateOrUpdateRequest
     )]
     private string $name;
 
+    /**
+     * The type of the property.
+     *
+     * @var string
+     */
     #[Assert\Choice(['property', 'parking_space'], message: 'Invalid property type: {{ value }}')]
     private string $type;
 
+    /**
+     * The ID of the parent property.
+     *
+     * @var int|null
+     */
     #[SerializedName("parent_id")]
     private ?int $parentId = null;
 
+    /**
+     * Constructor to initialize the CreateOrUpdateRequest object.
+     *
+     * @param string $name The name of the property.
+     * @param string $type The type of the property.
+     * @param int|null $parentId The ID of the parent property.
+     */
     public function __construct(string $name, string $type, ?int $parentId = null)
     {
         $this->name = $name;
@@ -29,16 +54,31 @@ class CreateOrUpdateRequest
         $this->parentId = $parentId;
     }
 
+    /**
+     * Gets the name of the property.
+     *
+     * @return string The property name.
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Gets the type of the property.
+     *
+     * @return string The property type.
+     */
     public function getType(): string
     {
         return $this->type;
     }
 
+    /**
+     * Gets the ID of the parent property.
+     *
+     * @return int|null The parent property ID.
+     */
     public function getParentId(): ?int
     {
         return $this->parentId;

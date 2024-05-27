a92f150cc93515ee3fda8aecd51482901e04d6f2
Andres Orav <andres.orav@gmail.com>
2024-05-28 01:46:24 +0300

    Comment entities

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -10,24 +10,53 @@ use App\Repository\PropertyRepository;
 #[ORM\Entity(repositoryClass: PropertyRepository::class)]
 class Property
 {
+    /**
+     * The unique identifier of the property.
+     *
+     * @var int|null
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    // Name uniqueness is checked in the code to allow soft delete
+    /**
+     * The name of the property.
+     *
+     * @var string
+     */
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
+    /**
+     * The type of the property.
+     *
+     * @var int
+     */
     #[ORM\Column(type: 'smallint')]
     private $type;
 
+    /**
+     * The date and time when the property was created.
+     *
+     * @var \DateTime
+     */
     #[ORM\Column(type: 'datetime')]
     private $created;
 
+    /**
+     * The date and time when the property was last modified.
+     *
+     * @var \DateTime
+     */
     #[ORM\Column(type: 'datetime')]
     private $modified;
 
+    /**
+     * The status of the property.
+     *
+     * @var int
+     */
     #[ORM\Column(type: 'smallint')]
     private $status;
 
@@ -38,54 +67,108 @@ class Property
         $this->status = PropertyStatus::ACTIVE->value;
     }
 
+    /**
+     * Gets the ID of the property.
+     *
+     * @return int|null The property ID.
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Gets the name of the property.
+     *
+     * @return string|null The property name.
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * Sets the name of the property.
+     *
+     * @param string $name The property name.
+     * @return self
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * Gets the type of the property.
+     *
+     * @return PropertyType The property type.
+     */
     public function getType(): PropertyType
     {
         return PropertyType::from($this->type);
     }
 
+    /**
+     * Sets the type of the property.
+     *
+     * @param PropertyType $type The property type.
+     * @return self
+     */
     public function setType(PropertyType $type): self
     {
         $this->type = $type->value;
         return $this;
     }
 
+    /**
+     * Gets the creation date of the property.
+     *
+     * @return \DateTimeInterface|null The creation date.
+     */
     public function getCreated(): ?\DateTimeInterface
     {
         return $this->created;
     }
 
+    /**
+     * Gets the modification date of the property.
+     *
+     * @return \DateTimeInterface|null The modification date.
+     */
     public function getModified(): ?\DateTimeInterface
     {
         return $this->modified;
     }
 
+    /**
+     * Sets the modification date of the property.
+     *
+     * @param \DateTimeInterface $modified The modification date.
+     * @return self
+     */
     public function setModified(\DateTimeInterface $modified): self
     {
         $this->modified = $modified;
         return $this;
     }
 
+    /**
+     * Gets the status of the property.
+     *
+     * @return PropertyStatus The property status.
+     */
     public function getStatus(): PropertyStatus
     {
         return PropertyStatus::from($this->status);
     }
 
+    /**
+     * Sets the status of the property.
+     *
+     * @param PropertyStatus $status The property status.
+     * @return self
+     */
     public function setStatus(PropertyStatus $status): self
     {
         $this->status = $status->value;
--- src/Entity/PropertyRelation.php
+++ src/Entity/PropertyRelation.php
@@ -6,28 +6,59 @@ use App\Enum\PropertyStatus;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PropertyRelationRepository;
 
+/**
+ * Represents a relation between properties.
+ */
 #[ORM\Entity(repositoryClass: PropertyRelationRepository::class)]
 class PropertyRelation
 {
+    /**
+     * The child property in the relation.
+     *
+     * @var Property|null
+     */
     #[ORM\Id]
     #[ORM\ManyToOne(targetEntity: Property::class)]
     #[ORM\JoinColumn(name: 'property_id', referencedColumnName: 'id')]
     private $property;
 
+    /**
+     * The parent property in the relation.
+     *
+     * @var Property|null
+     */
     #[ORM\Id]
     #[ORM\ManyToOne(targetEntity: Property::class)]
     #[ORM\JoinColumn(name: 'parent_id', referencedColumnName: 'id')]
     private $parent;
 
+    /**
+     * The date and time when the relation was created.
+     *
+     * @var \DateTime
+     */
     #[ORM\Column(type: 'datetime')]
     private $created;
 
+    /**
+     * The date and time when the relation was last modified.
+     *
+     * @var \DateTime
+     */
     #[ORM\Column(type: 'datetime')]
     private $modified;
 
+    /**
+     * The status of the relation.
+     *
+     * @var int
+     */
     #[ORM\Column(type: 'smallint')]
     private $status;
 
+    /**
+     * Constructor to initialize the creation and modification dates and set the status to active.
+     */
     public function __construct()
     {
         $this->created = new \DateTime();
@@ -35,49 +66,98 @@ class PropertyRelation
         $this->status = PropertyStatus::ACTIVE->value;
     }
 
+    /**
+     * Gets the child property of the relation.
+     *
+     * @return Property|null The child property.
+     */
     public function getProperty(): ?Property
     {
         return $this->property;
     }
 
+    /**
+     * Sets the child property of the relation.
+     *
+     * @param Property $property The child property.
+     * @return self
+     */
     public function setProperty(Property $property): self
     {
         $this->property = $property;
         return $this;
     }
 
+    /**
+     * Gets the parent property of the relation.
+     *
+     * @return Property|null The parent property.
+     */
     public function getParent(): ?Property
     {
         return $this->parent;
     }
 
+    /**
+     * Sets the parent property of the relation.
+     *
+     * @param Property|null $parent The parent property.
+     * @return self
+     */
     public function setParent(?Property $parent): self
     {
         $this->parent = $parent;
         return $this;
     }
 
+    /**
+     * Gets the creation date of the relation.
+     *
+     * @return \DateTimeInterface|null The creation date.
+     */
     public function getCreated(): ?\DateTimeInterface
     {
         return $this->created;
     }
 
+    /**
+     * Gets the modification date of the relation.
+     *
+     * @return \DateTimeInterface|null The modification date.
+     */
     public function getModified(): ?\DateTimeInterface
     {
         return $this->modified;
     }
 
+    /**
+     * Sets the modification date of the relation.
+     *
+     * @param \DateTimeInterface $modified The modification date.
+     * @return self
+     */
     public function setModified(\DateTimeInterface $modified): self
     {
         $this->modified = $modified;
         return $this;
     }
 
+    /**
+     * Gets the status of the relation.
+     *
+     * @return PropertyStatus The relation status.
+     */
     public function getStatus(): PropertyStatus
     {
         return PropertyStatus::from($this->status);
     }
 
+    /**
+     * Sets the status of the relation.
+     *
+     * @param PropertyStatus $status The relation status.
+     * @return self
+     */
     public function setStatus(PropertyStatus $status): self
     {
         $this->status = $status->value;
